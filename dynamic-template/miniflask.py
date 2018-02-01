9298bf8863a8c5d9a1c40d296f5b7e84b49b5140
@jesusq <jesusq@github.com>
2018-01-31 23:42:50 -0700

    Get index.html passed through Flask & static files connected
    Also update .gitignore & extract JSON data about images in static html

---
+++ dynamic-template/miniflask.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
